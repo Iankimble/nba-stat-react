0d2b73cb8ee8fad7b2d830d45e58f14525a3208a
iankimble <ian.kimble571@gmail.com>
2019-08-19 13:57:27 -0400

    Updates

---
+++ precache-manifest.154f3799b6666c5e82fe00280508f0ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb2a951f1664e4f20e7b0c202ee1f2e3",
+    "url": "/nba-stat-react/index.html"
+  },
+  {
+    "revision": "f7d6ef9954e174db44c1",
+    "url": "/nba-stat-react/static/css/main.dd53a303.chunk.css"
+  },
+  {
+    "revision": "fe7c711aa1350031799c",
+    "url": "/nba-stat-react/static/js/2.fee34a4d.chunk.js"
+  },
+  {
+    "revision": "f7d6ef9954e174db44c1",
+    "url": "/nba-stat-react/static/js/main.497c842b.chunk.js"
+  },
+  {
+    "revision": "d2e345f9b759b105e950",
+    "url": "/nba-stat-react/static/js/runtime~main.f80fcfbe.js"
+  },
+  {
+    "revision": "a95a748c5a33e59dabffbe669a2d1a2f",
+    "url": "/nba-stat-react/static/media/landing.a95a748c.png"
+  },
+  {
+    "revision": "760515c3577383077f8971920957ac9e",
+    "url": "/nba-stat-react/static/media/nbasl.760515c3.png"
+  }
+]);
